9004eb612a8339601bad83a81bedbce47981eeca
UmarADev <umargithub03@gmail.com>
2024-06-05 22:14:51 +0500

    Done

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+Console.WriteLine("Please give me two numbers:");
+int firstNum = Convert.ToInt32(Console.ReadLine());
+int secondNum = Convert.ToInt32(Console.ReadLine());
+
+for (int i = firstNum; i <= secondNum; i++)
+{
+    for (int a = 1; a < 11; a++)
+    {
+        Console.WriteLine($"{i} * {a} = {a*i} \t");
+    }
+    Console.WriteLine();
+}
