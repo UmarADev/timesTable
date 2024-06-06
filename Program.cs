bbe178f81fdc96723bd47be933bc27aca1a4cd96
UmarADev <umargithub03@gmail.com>
2024-06-06 21:58:56 +0500

    Correct Mistake

--- Program.cs
+++ Program.cs
@@ -8,7 +8,7 @@ for (int i = firstNum; i <= secondNum; i++)
 {
     for (int a = 1; a < 11; a++)
     {
-        Console.WriteLine($"{i} * {a} = {a*i} \t");
+        Console.WriteLine($"{i} * {a} = {a*i}");
     }
     Console.WriteLine();
 }
